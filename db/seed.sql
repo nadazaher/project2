0e2a47394f290876e280be79a6e2e7d8f4224aec
NADA ZAHER <zahernada94@gmail.com>
2018-08-06 23:14:45 -0400

    seeding pics

--- db/seed.sql
+++ db/seed.sql
@@ -36,7 +36,49 @@ INSERT INTO restaurant (name, location, cuisine, photo) VALUES
 ( 'Buddakan',
     'Chelsea',
        'Chinese',
-            'https://static1.squarespace.com/static/565a9019e4b0a63eb2cc2b7d/t/5a20438b0d9297af42ddfc84/1512063892268/buddukan');
+            'https://static1.squarespace.com/static/565a9019e4b0a63eb2cc2b7d/t/5a20438b0d9297af42ddfc84/1512063892268/buddukan'),
+
+    
+( 'Red Farm', 
+    'Upper West Side',
+        'Chinese',
+            'http://www.glenwoodnyc.com/manhattan-living/wp-content/uploads/2011/09/red-farm-nyc-dim-sum-restaurant-village.jpg'),
+
+( 'Eataly', 
+    'Flatiron',
+        'Italian',
+        'https://static1.squarespace.com/static/565a9019e4b0a63eb2cc2b7d/t/5a20438b0d9297af42ddfc84/1512063892268/buddukan'),
+
+( 'Junoon', 
+    'Flatiron',
+        'Indian',
+        'https://static1.squarespace.com/static/565a9019e4b0a63eb2cc2b7d/t/5a20438b0d9297af42ddfc84/1512063892268/buddukan'),
+
+('Manousheh', 
+    'Greenwich Village',
+        'Lebanese',
+        'https://static1.squarespace.com/static/565a9019e4b0a63eb2cc2b7d/t/5a20438b0d9297af42ddfc84/1512063892268/buddukan'), 
+
+('La Esquina', 
+    'East Village',
+        'Mexican',
+        'https://static1.squarespace.com/static/565a9019e4b0a63eb2cc2b7d/t/5a20438b0d9297af42ddfc84/1512063892268/buddukan'), 
+
+('aRoqa',
+    'Chelsea',
+        'Indian',
+        'https://static1.squarespace.com/static/565a9019e4b0a63eb2cc2b7d/t/5a20438b0d9297af42ddfc84/1512063892268/buddukan'),
+
+('Taqueria Diana',
+    'Midtown',
+        'Mexican',
+        'https://static1.squarespace.com/static/565a9019e4b0a63eb2cc2b7d/t/5a20438b0d9297af42ddfc84/1512063892268/buddukan'),
+
+('Babbo',
+    'Greenwich Village',
+        'Italian',
+        'https://static1.squarespace.com/static/565a9019e4b0a63eb2cc2b7d/t/5a20438b0d9297af42ddfc84/1512063892268/buddukan');
+
 
 DELETE FROM reviews;
 
@@ -82,5 +124,53 @@ INSERT INTO reviews (restaurant_id, author, content) VALUES
  ),
 (7, 'Jennifer',
  'Beautiful decor and ambiance. Food was delectable. Service was very helpful with suggestions and also attentive. '
- );
+ ),
+
+(8, 'Judi',
+ 'The vibe of this place is like no ordinary Chinese restaurant it is very laid back and different. The decor reminds you if you were at a barn or farm. The food and flavors are quite unique and satisfying. '
+ ),
+ (8, 'Jim',
+ 'Sunday night dinner on weekend trip to NY City. Restrauant was recommended by a friend in the city. The food and presentation were superb. Great attention to detail.'
+ ),
+(9, 'Ricardo',
+ 'There are multiple places within Eataly. The rooftop bar is lush, with tasty cocktails and good eats. '
+ ),
+ (9, 'Tatiana',
+ 'I just love everything about Eataly. Fabulous quality food, incredible selection. Knowledgeable expert staff. Fantastic range of produce, fresh daily. Wonderful Cafés, bars and restaurants.'
+ ),
+(10, 'Raj',
+ 'Came here for restaurant week and was able to enjoy a tasting menu. Very elegant Indian authentic restaurants. The flavors are not overdone, but just right.'
+ ),
+ (10, 'Erin',
+ 'The salmon starter was extremely flavorsome. But the main course was outstanding. It was essentially a vegetable curry with rice and chapati but was by far the best curry I’ve ever had. It was season to perfection and had a perfect balance of flavors.'
+ ),
+ (11, 'Mahmoud',
+ 'This cute little spot has the best manakeesh in NYC. As an extra pointer, try the labne, and tell them to add it to your zaatar manoushe, its a union thats meant to be.'
+ ),
+(11, 'Dana',
+ 'Honestly some of the best zataar and jibneh Ive ever had and Ive had plenty in the Middle East.'
+ ),
+ (12, 'Anita',
+ 'I love the vibes in this place. The servers are really nice, and the food is fantastic. Im a big fan of mexican, and this definitely stands up to a lot of the other mex places in the area.'
+ ),
+  (12, 'Carlos',
+ 'Best guacamole and best tacos in the city and trust me Ive tried many.'
+ ),
+  (13, 'Farah',
+ 'Tapas-style Indian restaurant. A very good experience and I would certainly go back again, probably mainly for their apps and tasting menu.'
+  ),
+(13, 'Akrati',
+ 'This was a fun and interesting experience for us!  I enjoyed their Indian tapas/fusion vibe and cute cocktails.'
+  ),
+(14, 'Alejandro',
+ 'I come here for the nachos and cocktails. Never a bad choice.'
+  ),
+(14, 'Lara',
+ 'Cute spot. Trendy. Food is decent. Would go back if I wanted some tacos.'
+  ),
+(15, 'Enrico',
+ 'Babbo is a solid restaurant. The price point is reasonable considering the quality and Michelin star rating as well. The grilled octopus is a winner. The pastas were good and sizeable in portions.'
+  ),
+(15, 'Isabel',
+ 'Exceptional on every level, from the welcoming team, to the waiters, support staff and of course - the exceptional food. Loved the goat cheese tortellini and asparagus ravioli which could easily be a meal by themselves.');
 
